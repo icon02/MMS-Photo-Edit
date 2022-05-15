3bf300f85c87fe686fbbbf5708431f9829fea768
icon02 <nico.layr@gmx.at>
2022-05-15 21:17:29 +0200

    fixes

--- MMS-Photo-Edit-Backend/src/main/java/io/github/icon02/MMSPhotoEditBackend/mapper/SelectionToRasterMapper.java
+++ MMS-Photo-Edit-Backend/src/main/java/io/github/icon02/MMSPhotoEditBackend/mapper/SelectionToRasterMapper.java
@@ -10,31 +10,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class SelectionToRasterMapper {
 
-    public Boolean[][] getRaster(Object selection, int reqWidth, int reqHeight) {
-
+    public Boolean[][] getRaster(Object selection, Integer reqWidth, Integer reqHeight) {
+        if(reqWidth == null || reqHeight == null) return null;
         Boolean[][] raster = null;
 
         if(selection instanceof CircleSelection)
-            raster = getRasterFromCircle((CircleSelection) selection);
+            raster = getRasterFromCircle((CircleSelection) selection, reqWidth, reqHeight);
         else if(selection instanceof RectSelection)
-            raster = getRasterFromRect((RectSelection) selection);
+            raster = getRasterFromRect((RectSelection) selection, reqWidth, reqHeight);
         else if(selection instanceof FreehandSelection)
-            raster = getRasterFromFreehand((FreehandSelection) selection);
+            raster = getRasterFromFreehand((FreehandSelection) selection, reqWidth, reqHeight);
 
         return new SubSampler<Boolean>().resize(raster, reqWidth, reqHeight, Boolean.class);
     }
 
-    private Boolean[][] getRasterFromCircle(CircleSelection selection) {
+    private Boolean[][] getRasterFromCircle(CircleSelection selection, int reqWidth, int reqHeight) {
         // TODO
         return null;
     }
 
-    private Boolean[][] getRasterFromRect(RectSelection selection) {
+    private Boolean[][] getRasterFromRect(RectSelection selection, int reqWidth, int reqHeight) {
         // TODO
         return null;
     }
 
-    private Boolean[][] getRasterFromFreehand(FreehandSelection selection) {
+    private Boolean[][] getRasterFromFreehand(FreehandSelection selection, int reqWidth, int reqHeight) {
         // TODO
         return null;
     }
