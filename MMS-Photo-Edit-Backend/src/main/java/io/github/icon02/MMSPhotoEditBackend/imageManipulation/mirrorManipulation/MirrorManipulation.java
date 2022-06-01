3fa52bc9da32b469469b5a00f1253f7540d25f17
icon02 <nico.layr@gmx.at>
2022-06-01 14:37:04 +0200

    bugfix #3

--- MMS-Photo-Edit-Backend/src/main/java/io/github/icon02/MMSPhotoEditBackend/imageManipulation/mirrorManipulation/MirrorManipulation.java
+++ MMS-Photo-Edit-Backend/src/main/java/io/github/icon02/MMSPhotoEditBackend/imageManipulation/mirrorManipulation/MirrorManipulation.java
@@ -26,24 +26,26 @@ public class MirrorManipulation implements ImageFilter {
     /**
      * Mirror (part of) the image.
      *
-     * @param image             Original image.
-     * @param selectionRaster   Defines the part, that will be mirrored, if not null;
-     * @return                  Mirrored image.
+     * @param image           Original image.
+     * @param selectionRaster Defines the part, that will be mirrored, if not null;
+     * @return Mirrored image.
      */
     private BufferedImage mirrorImage(BufferedImage image, Boolean[][] selectionRaster) {
         BufferedImage newImg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-        for (int i = 0; i < newImg.getHeight(); i++ ) {
+        for (int i = 0; i < newImg.getHeight(); i++) {
             for (int j = 0; j < newImg.getWidth(); j++) {
 
                 Color c = new Color(image.getRGB(j, i));
-                if (selectionRaster == null || selectionRaster[i][j] != null && selectionRaster[i][j]) {
+                if (selectionRaster == null ||
+                        selectionRaster.length <= i || selectionRaster[i].length <= j ||
+                        selectionRaster[i][j] != null && selectionRaster[i][j]) {
+                    //
                     if (direction == Direction.VERTICAL)
                         newImg.setRGB((newImg.getWidth() - 1) - j, i, c.getRGB());
                     else if (direction == Direction.HORIZONTAL)
                         newImg.setRGB(j, (newImg.getHeight() - 1) - i, c.getRGB());
-                }
-                else newImg.setRGB(i, j, c.getRGB());
+                } else newImg.setRGB(i, j, c.getRGB());
 
             }
         }
